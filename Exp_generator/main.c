8b475b736cb0ba41689a99734452392465b28787
Sidi Chang <sidichang@Sidis-MacBook-Pro.local>
2015-03-25 23:52:00 -0400

    Initial Commit

---
+++ Exp_generator/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Exp_generator
+//
+//  Created by Sidi Chang on 3/25/15.
+//  Copyright (c) 2015 Sidi Chang. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
